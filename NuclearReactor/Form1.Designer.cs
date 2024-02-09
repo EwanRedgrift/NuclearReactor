84ba18491a4ac78055f9d5b809785d81e613f765
EwanRedgrift <ewanredg500@ed.amdsb.ca>
2024-02-09 14:05:29 -0500

    Add project files.

---
+++ NuclearReactor/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace NuclearReactor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.rizzColorLabel = new System.Windows.Forms.Label();
+            this.fanumColarLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fanumlabel = new System.Windows.Forms.Label();
+            this.lableLAble = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AccessibleName = "titleText";
+            this.label1.BackColor = System.Drawing.Color.DarkMagenta;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 35.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(853, 103);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ewan\'s Redgrift\'s application of visual studo";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // rizzColorLabel
+            // 
+            this.rizzColorLabel.BackColor = System.Drawing.Color.Red;
+            this.rizzColorLabel.Location = new System.Drawing.Point(166, 163);
+            this.rizzColorLabel.Name = "rizzColorLabel";
+            this.rizzColorLabel.Size = new System.Drawing.Size(145, 125);
+            this.rizzColorLabel.TabIndex = 1;
+            this.rizzColorLabel.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // fanumColarLabel
+            // 
+            this.fanumColarLabel.BackColor = System.Drawing.Color.Red;
+            this.fanumColarLabel.Location = new System.Drawing.Point(594, 163);
+            this.fanumColarLabel.Name = "fanumColarLabel";
+            this.fanumColarLabel.Size = new System.Drawing.Size(145, 125);
+            this.fanumColarLabel.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "meatlloadButton";
+            this.button1.Font = new System.Drawing.Font("Jokerman", 41.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(261, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(396, 106);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Meat loaf";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AccessibleName = "rizzLabel";
+            this.label4.BackColor = System.Drawing.Color.DarkRed;
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 30.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label4.Location = new System.Drawing.Point(181, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 35);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Rizz";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // fanumlabel
+            // 
+            this.fanumlabel.AccessibleName = "fanumLable";
+            this.fanumlabel.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.fanumlabel.Font = new System.Drawing.Font("Monotype Corsiva", 30.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.fanumlabel.Location = new System.Drawing.Point(610, 128);
+            this.fanumlabel.Name = "fanumlabel";
+            this.fanumlabel.Size = new System.Drawing.Size(118, 35);
+            this.fanumlabel.TabIndex = 5;
+            this.fanumlabel.Text = "Kai";
+            this.fanumlabel.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lableLAble
+            // 
+            this.lableLAble.AccessibleName = "labelLabel";
+            this.lableLAble.BackColor = System.Drawing.Color.White;
+            this.lableLAble.Font = new System.Drawing.Font("Monotype Corsiva", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lableLAble.Location = new System.Drawing.Point(448, 228);
+            this.lableLAble.Name = "lableLAble";
+            this.lableLAble.Size = new System.Drawing.Size(112, 60);
+            this.lableLAble.TabIndex = 6;
+            this.lableLAble.Text = "label6";
+            this.lableLAble.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.lableLAble.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(901, 464);
+            this.Controls.Add(this.lableLAble);
+            this.Controls.Add(this.fanumlabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.fanumColarLabel);
+            this.Controls.Add(this.rizzColorLabel);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Monotype Corsiva", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Robot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label rizzColorLabel;
+        private System.Windows.Forms.Label fanumColarLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label fanumlabel;
+        private System.Windows.Forms.Label lableLAble;
+    }
+}
+
---
+++ NuclearReactor/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace NuclearReactor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rizzColorLabel.BackColor = Color.Black;
+            fanumColarLabel.BackColor = Color.Black;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            rizzColorLabel.BackColor = Color.Indigo;
+            fanumColarLabel.BackColor = Color.IndianRed;
+            
+            Refresh();
+            Thread.Sleep(1000);
+
+            rizzColorLabel.BackColor = Color.Red;
+            fanumColarLabel.BackColor = Color.Red;
+            
+            rizzColorLabel.Location = new Point(1,1);
+
+            lableLAble.Text = "Rizzin' XD";
+            lableLAble.ForeColor = Color.OrangeRed;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
